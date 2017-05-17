56d72fe70f546afe7e01ceac9dc348daac5bdc65
TheBestDon <donatas_bakanas@yahoo.com>
2017-05-18 00:44:43 +0300

    add geolocatio support via Geolocation API

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 
-const {generateMessage} = require('./message');
+const {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
     it('should generate correct message object', () => {
@@ -11,5 +11,17 @@ describe('generateMessage', () => {
         expect(message.createdAt).toBeA('number');
         expect(message).toInclude({from, text});
     });
+});
 
+describe('generateLocationMessage', () => {
+    it('should generate correct location object', () => {
+        var from = 'Don';
+        var latitude = 1;
+        var longitude = 1;
+        var url = 'https://www.google.com/maps?q=1,1';
+        var message = generateLocationMessage(from, latitude, longitude);
+
+        expect(message.createdAt).toBeA('number');
+        expect(message).toInclude({from, url});
+    });
 });
