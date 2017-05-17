079b7ea6f17b8696cdf30ed8792048ea273ab5e8
TheBestDon <donatas_bakanas@yahoo.com>
2017-05-17 17:47:57 +0300

    initial commit

---
+++ server/server.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
+
+var publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+app.use(express.static(publicPath));
+
+io.on('connection', (socket) => {
+    console.log('new user');
+
+socket.on('createMessage', (message) => {
+    console.log('New message created', message);
+    io.emit('message', {
+        from: message.from,
+        text: message.text,
+        createdAt: new Date().getTime()
+    });
+});
+
+socket.on('disconnect', () => {
+    console.log('Client disconnected'); 
+});
+});
+
+server.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
+
+module.exports = {app};
